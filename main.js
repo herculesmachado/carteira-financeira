d7680de726e1de77fcd618ac42a02544167cee66
Hércules Machado <herculesprogramador911@gmail.com>
2022-08-03 22:25:09 -0300

    Projeto Finalizado

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //Este objeto tem a funcionalidade de ligar e desligar o modal
 const modal = {
+
     open() {
         //Abrir modal
         document.querySelector('.modal-overlay').classList.add('active')
@@ -11,46 +12,72 @@ const modal = {
     }
 }
 
+// Este objeto armazena as informações na localStorage do browser
+const Storage = {
+    // Pegar
+    get() {
+        // parse é a funcionalidade que vai pegar a minhas strings e transformar elas em uma array ou objeto para array
+        return JSON.parse(localStorage.getItem('dev.finances: transaction')) || []
+    },
+
+    // Setar
+    set(transaction) {
+        //Ele vai transformar as minhas transaction em uma string
+        localStorage.setItem('dev.finances: Transaction', JSON.stringify(transaction))
+    }
+}
+
 // Meus dados da tabela
-const transaction = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/07/2022'
-    },
-    {
-        id: 2,
-        description: 'Criação website',
-        amount: 500000,
-        date: '14/05/2022'
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -5000,
-        date: '20/06/2022'
-    },
-    {
-        id: 4,
-        description: 'App',
-        amount: 200000,
-        date: '20/06/2022'
-    },
-]
 
 const Transaction = {
-    income() {
-        //Pegar todas as transções
-        // para cada transação, se for maior que 0 somar a uma varaivel e retornar a variavel
+    all: Storage.get(),
+
+    // Adiconar uma transação
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload()
     },
-    expense() {
-        //Adiciona as perdas
-        return 'aqui'
+
+    // remove uma transação (index) é uma imagem de formato circular com um traço vermelho em que toda vez que clicar nele a tabela é desfeita
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
     },
+
+
+    // Somando as contas
+    incomes() {
+        let income = 0
+        // Pega cada transação
+        Transaction.all.forEach(transaction => {
+            // Se ela for maior que zero
+            if (transaction.amount > 0) {
+                // Soma a uma variavel e retonar a variavel
+                income += transaction.amount
+            }
+        })
+        return income
+    },
+
+    // subtraindo as contas
+    expenses() {
+        let expense = 0
+        // Pega cada transação
+        Transaction.all.forEach(transaction => {
+            // Se ela for maior que zero
+            if (transaction.amount < 0) {
+                // Como estou subtraindo os sinais nesse caso são inverso, o mais para fronte e os 
+                expense += transaction.amount
+            }
+        })
+        return expense
+    },
+
+    // totalizando as
     total() {
-        //Adiciona o total
-        return 'discover'
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
@@ -63,24 +90,26 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
 
         //Elemento criado pelo HTML
         DOM.transactionContainer.appendChild(tr)
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         //verifica se o numero é maior que 0, se sim, é ganho(income), senão e perda(expense)
         const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
 
         const amount = Utils.formatCurrency(transaction.amount)
 
+        // Modelo padrão da tabela HTMl, quando toda vez for adicionar algo novo, esse esqueleto de tabela é feito assim gerando outra tabela da mesma forma
         const html = `
         <td class="description">${transaction.description}</td>
         <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
-            <img src="./assets/assets/minus.svg" alt="Remover Transação">
+            <img onclick="Transaction.remove(${index})" src="./assets/assets/minus.svg" alt="Remover Transação">
         </td>
         `
 
@@ -89,15 +118,34 @@ const DOM = {
 
     // Mostrar os valores atualizados 
     updateBalance() {
-        document.getElementById('incomeDisplay').innerHTML.Transaction.income()
+        // 
+        document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())
 
-        document.getElementById('expenseDisplay').innerHTML.Transaction.expense()
+        document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
 
-        document.getElementById('totalDisplay').innerHTML.Transaction.total()
+        document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+
+    clearTransaction() {
+        DOM.transactionContainer.innerHTML = ''
     }
 }
 
 const Utils = {
+    formatAmount(value) {
+        value = Number(value) * 100
+
+        return value
+    },
+
+    // Forma as datas
+    formatDate(date) {
+        const splittedDate = date.split('-')
+        return `${splittedDate[2]}/${splittedDate[1]} ${splittedDate[0]}`
+    },
+
+
     formatCurrency(value) {
         // To fazendo uma lógica na qual to forçando o dato (value) para ser um Number, e com isso to dizendo que se o (value) for menor que 0, add um -(negativo), caso não, não add nada
         const signal = Number(value) < 0 ? '-' : ''
@@ -114,18 +162,112 @@ const Utils = {
             currency: 'BRL',
 
         })
-        
+
         return signal + value
 
     }
 }
 
-//Para cade elementos de transações está rodando uma funcionalidade, ai esta funcionalidade vai rodar a transação do momento que é o (transaction)
-transaction.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+const Form = {
 
-// Chamanda do updateBalance
-DOM.updateBalance()
+    // Esta forma eu to linkando com os inputs lá do HTML
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+
+    // Toda vez em que eu acessar o getValue eu vou ter todos os valores
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value,
+        }
+    },
+
+    // Vai validar os dados
+    validateFields() {
+        const { description, amount, date } = Form.getValues()
+
+        if (description.trim() === '' ||
+            amount.trim() === '' ||
+            date.trim() === '') {
+
+            // Lança um erro caso o usuario não coloque todos os dados
+            throw new Error('Por favor, preencha todas as informações')
+
+        }
+
+    },
+
+    // Formata os valores adicionadas
+    formatValues() {
+        let { description, amount, date } = Form.getValues()
+
+        amount = Utils.formatAmount(amount)
+
+        date = Utils.formatDate(date)
+
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+
+    // Salva a transação
+    saveTransaction(transaction) {
+        transaction.add(transaction)
+    },
+
+    // Limpando os campos a transação
+    clearFields() {
+        Form.description.value = ''
+        Form.amount.value = ''
+        Form.date.value = ''
+    },
+
+    submit(event) {
+        // Quando chegar aqui ele não vai gerar varios numeros ao lado da URL
+        event.preventDefault()
+
+        // Aqui eu estou tratando o erro caso a pessoa não tenha colocado todas as informações do formulario, e este erro esta sendo lançando uma nova mensagem no validateFields -> if -> throw.
+        try {
+            // Verificar se todas as informações foram preenchidas
+            Form.validateFields()
+            // formatar os dados para salvar
+            const transaction = Form.formatValues()
+            // Salvar
+            Transaction.add(transaction)
+            // apagar os dados do formulario
+            Form.clearFields()
+            // modal feche
+            modal.close()
+        } catch (e) {
+            alert("Error: " + e.message)
+        }
+    }
+}
+
+const App = {
+    init() {
+
+        Transaction.all.forEach(DOM.addTransaction)
+
+        DOM.updateBalance()
+
+        Storage.set(Transaction.all)
+
+    },
+
+    // Toda vez que acontece algo na transaction ele recarrega, assim dando um reload()
+    reload() {
+        DOM.clearTransaction()
+
+        App.init()
+    }
+}
+App.init()
+
 
 
